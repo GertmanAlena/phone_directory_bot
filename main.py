a179b4cebd72b6bd28b2be4056a1c2bed4ac2dca
Alena <gertmanea@mail.ru>
2022-10-09 20:25:09 +0300

    .\main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+﻿from telegram import Update, Bot
+from telegram.ext import Updater, CommandHandler, Filters, MessageHandler, ConversationHandler
+from config import TOKEN
+from anecAPI import anecAPI
+import telebot
+
+bot = telebot.TeleBot(TOKEN)
+
+
