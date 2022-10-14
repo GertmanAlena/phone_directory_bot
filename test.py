10e34b6a1b55cd2e572633c87e5e15ff7f665352
Alena <gertmanea@mail.ru>
2022-10-14 17:10:15 +0300

    .\test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+﻿def correct_name():
+    """Проверка правильности ввода имени"""
+    while True:
+        name = input("Введите имя-> ")
+        if name.isalpha() and len(name) < 10:
+            return name.capitalize()
+        else:
+            # logger.data_name("Введено не корректное имя или фамилия.")
+            print("\033[31m {}\033[0m" .format("Введены не корректные данные"))
+            
